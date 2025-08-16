a719e1a2387b5b6a9f3f91b15cc558589d0457f8
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2025-08-16 15:03:55 +0300

    Create IUnitOfWork Interface and UnitOfWork Class

---
+++ MyFirstMVCAPP/Repository/Base/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using MyFirstMVCAPP.Models;
+
+namespace MyFirstMVCAPP.Repository.Base
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IRepository<Category> Categories { get; }
+        IRepository<Item> Items { get; }
+        IRepository<Employee> Employees { get; }
+        int ComitChanges();
+    }
+}
---
+++ MyFirstMVCAPP/Repository/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using MyFirstMVCAPP.Data;
+using MyFirstMVCAPP.Models;
+using MyFirstMVCAPP.Repository.Base;
+
+namespace MyFirstMVCAPP.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Categories = new MainRepository<Category>(_context);
+            Items = new MainRepository<Item>(_context);
+            Employees = new MainRepository<Employee>(_context);
+        }
+        private readonly AppDbContext _context;
+        public IRepository<Category> Categories { get; private set; }
+
+        public IRepository<Item> Items { get; private set; }
+
+        public IRepository<Employee> Employees { get; private set; }
+
+        public int ComitChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+        }
+    }
+}
